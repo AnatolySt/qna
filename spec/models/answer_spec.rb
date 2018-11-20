25f39e06c6bdee9cfcf4f9682d403b298721724e
AnatolySt <an@webbery.pro>
2018-11-20 13:39:05 +0300

    Ajaxify Additions 0.3

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Answer, type: :model do
   let(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
   let!(:answer) { create(:answer, question: question ) }
-  let!(:other_answer) { create(:answer, question: question ) }
+  let!(:best_answer) { create(:answer, question: question, best: true ) }
 
   it { should belong_to(:question)}
   it { should validate_presence_of(:body) }
@@ -15,8 +15,8 @@ RSpec.describe Answer, type: :model do
   end
 
   it 'mark only one best answer ' do
-    other_answer.best_switch
-    expect(answer).to_not be_best
+    answer.best_switch
+    expect(best_answer.reload).to_not be_best
   end
 
 end
