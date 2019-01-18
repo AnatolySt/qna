627b40b04fb77c26825ee39a4655b84a12b66ada
AnatolySt <an@webbery.pro>
2019-01-18 20:19:26 +0300

    Refactor: omniauth_callbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,20 +1,29 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def vkontakte
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
+  before_action :set_provider, only: [:vkontakte, :github]
+  before_action :make_oauth, only: [:vkontakte, :github]
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Vkontakte') if is_navigational_format?
-    end
+  def vkontakte
   end
 
   def github
+  end
+
+  private
+
+  def set_provider
+    @provider = request.env['omniauth.auth'].provider.capitalize
+  end
+
+  def make_oauth
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: @provider) if is_navigational_format?
+    else
+      redirect_to new_user_session_path
+      flash[:alert] = "Произошла ошибка, попробуйте еще раз." if is_navigational_format?
     end
   end
 
